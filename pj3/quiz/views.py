5b3bcb1562df675d488874632c5f60f55069d522
bxgj <13600363246@139.com>
2016-01-21 01:23:45 +0800

    pj3

--- pj3/quiz/views.py
+++ pj3/quiz/views.py
@@ -4,26 +4,28 @@ from django.views.generic import ListView, DetailView, CreateView,UpdateView
 from django.core.urlresolvers import reverse_lazy, reverse
 
 class QuestionList(ListView):
-	model=Question
-	template_name='quiz/question_list.html'
-	context_object_name='question_list'
+    model=Question
+    template_name='quiz/question_list.html'
+    context_object_name='question_list'
 
 class QuestionDetail(DetailView):
-	model=Question
-	template_name='quiz/question_detail.html'
-	context_object_name='question_object'
+    model=Question
+    template_name='quiz/question_detail.html'
+    context_object_name='question_object'
 
 class QuestionCreate(CreateView):
-	model=Question
-	fields='__all__'
-	template_name='quiz/question_edit.html'
-	def get_success_url(self):
-		return reverse('question-list')
+    model=Question
+    fields='__all__'
+    template_name='quiz/question_edit.html'
+    context_object_name='myquestion' #default is "object" or "question"
+    def get_success_url(self):
+        return reverse('question-list')
 
 class QuestionUpdate(UpdateView):
     models=Question
     fields=['question_text',]
     template_name='quiz/question_edit.html'
+    context_object_name='myquestion'
     def get_queryset(self):
         queryset=Question.objects.filter(pk=self.kwargs['pk'])
         # get return object, filter return queryset, here can not use get
