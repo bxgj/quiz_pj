22581c64c1b7f4a9254fa1bf4b6ad4fd907e48c6
bxgj <13600363246@139.com>
2016-01-21 01:17:46 +0800

    pj3

--- pj3/quiz/urls.py
+++ pj3/quiz/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from quiz.views import QuestionList, QuestionCreate, QuestionDetail
+from quiz.views import QuestionList, QuestionCreate, QuestionDetail,QuestionUpdate
 # from django.views.generic import TemplateView
 
 urlpatterns = [
     # url(r'', TemplateView.as_view(template_name='quiz/index.html'), name='index'),
     url(r'^$', QuestionList.as_view(), name='question-list'),
     url(r'^(?P<pk>[0-9]+)/$', QuestionDetail.as_view(), name='question-detail'),
+    url(r'^edit/(?P<pk>[0-9]+)/$', QuestionUpdate.as_view(), name='question-edit'),
     url(r'^new$', QuestionCreate.as_view(), name='question-new'),
 ]
--- pj3/quiz/views.py
+++ pj3/quiz/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from quiz.models import Question, Choice
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView,UpdateView
 from django.core.urlresolvers import reverse_lazy, reverse
 
 class QuestionList(ListView):
@@ -17,8 +17,16 @@ class QuestionCreate(CreateView):
 	model=Question
 	fields='__all__'
 	template_name='quiz/question_edit.html'
-	# success_url=reverse_lazy('question_list') # do not use this one, many problems
-	# whild call this class view , show the form to edit, form name is "form" as default
 	def get_success_url(self):
-		return reverse('question-list') 
-		# while success submit the form , jump to list page to advoid repeat submit
+		return reverse('question-list')
+
+class QuestionUpdate(UpdateView):
+    models=Question
+    fields=['question_text',]
+    template_name='quiz/question_edit.html'
+    def get_queryset(self):
+        queryset=Question.objects.filter(pk=self.kwargs['pk'])
+        # get return object, filter return queryset, here can not use get
+        return queryset
+    def get_success_url(self):
+        return reverse('question-list')
